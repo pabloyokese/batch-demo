d50bb790521314e206c56735701f5cb05b68742f
jurbano <jurbano@fintonic.com>
2018-01-02 01:16:08 +0100

    Transaccionalidad

--- src/main/java/com/example/demo/configuration/DemoConfiguration.java
+++ src/main/java/com/example/demo/configuration/DemoConfiguration.java
@@ -2,17 +2,24 @@ package com.example.demo.configuration;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.file.transform.FieldSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.validation.BindException;
+
+import java.util.List;
 
 @Configuration
 @EnableBatchProcessing
@@ -27,13 +34,45 @@ public class DemoConfiguration {
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-            .tasklet(new Tasklet() {
-                public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
-                    System.out.println("Reading...");
-                    return null;
-                }
-            })
+            .<Person,Person>chunk(2)
+            .reader(csvReader())
+            .writer(writer())
             .build();
+    }
+
+    @Bean
+    public ItemReader<Person> csvReader() {
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setNames(new String[]{"name", "age"});
+
+        DefaultLineMapper<Person> lineMapper = new DefaultLineMapper<>();
+        lineMapper.setLineTokenizer(lineTokenizer);
+        lineMapper.setFieldSetMapper(personMapper());
+
+        FlatFileItemReader<Person> itemReader = new FlatFileItemReader<>();
+        itemReader.setResource(new ClassPathResource("names.csv"));
+        itemReader.setLineMapper(lineMapper);
+        return itemReader;
+    }
+
+    @Bean
+    public FieldSetMapper<Person> personMapper() {
+        return new FieldSetMapper<Person>() {
+            @Override
+            public Person mapFieldSet(FieldSet fieldSet) throws BindException {
+                return new Person(fieldSet.readString("name"),fieldSet.readString("age"));
+            }
+        };
+    }
+
+    @Bean
+    public ItemWriter<Person> writer() {
+        return new ItemWriter<Person>() {
+            @Override
+            public void write(List<? extends Person> people) throws Exception {
+                System.out.println(String.join(", ",people.stream().map(Person::getName).toArray(String[]::new)));
+            }
+        };
     }
 
     @Bean
@@ -42,5 +81,30 @@ public class DemoConfiguration {
             .incrementer(new RunIdIncrementer())
             .start(step1)
             .build();
+    }
+
+    private class Person {
+        private String name,age;
+
+        public Person(String name, String age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getAge() {
+            return age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setAge(String age) {
+            this.age = age;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
