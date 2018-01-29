e2cf93597a76f4856e977b1c3408e3629b7f1087
jurbano <jurbano@fintonic.com>
2018-01-29 09:15:19 +0100

    Administración web

--- src/main/java/com/example/demo/BatchDemoApplication.java
+++ src/main/java/com/example/demo/BatchDemoApplication.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
-import com.example.demo.configuration.DemoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
 
+@EnableTask
 @SpringBootApplication
 public class BatchDemoApplication {
 
